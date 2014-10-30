1075ab107ade52ce00ae39ca0a205e5213292ab6
gnlaredo <gnlaredo@gmail.com>
2014-10-30 12:55:39 -0300

    Ultima v1.0

---
+++ Entrega 6 OK/DDS-2014-Grupo3/Formularios/Form4.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DDS_2014_Grupo4.Clases.Pantallitas
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        public Form4(SqlDataReader reader, SqlConnection conn)
+        {
+            InitializeComponent();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["Apodo"].ToString());
+            }
+            reader.Close();
+            conn.Close(); 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.textBox2.Text = listBox1.SelectedItems[0].ToString();
+            string ConnStr = @"Data Source=localhost\SQLSERVER2008;Initial Catalog=GD2C2014;User ID=gd;Password=gd2014;Trusted_Connection=False;";
+
+            SqlConnection conn = new SqlConnection(ConnStr);
+            string sSel = string.Format("SELECT * FROM [GD2C2014].[dbo].[Jugador] j, [GD2C2014].[dbo].[Persona] p where j.Apodo = '{0}' and j.ID_Persona = p.ID_Persona", textBox2.Text);
+            
+            
+            SqlCommand cmd = new SqlCommand(sSel, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                textBox1.Text = reader["Nombre"].ToString();
+                textBox3.Text = reader["Handicap"].ToString();
+            }
+            reader.Close();
+            conn.Close(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.textBox2.Text = listBox1.SelectedItems[0].ToString();
+            string ConnStr = @"Data Source=localhost\SQLSERVER2008;Initial Catalog=GD2C2014;User ID=gd;Password=gd2014;Trusted_Connection=False;";
+
+            SqlConnection conn = new SqlConnection(ConnStr);
+            string sSel = string.Format("SELECT * FROM [GD2C2014].[dbo].[Jugador] j, [GD2C2014].[dbo].[Persona] p where j.Apodo = '{0}' and j.ID_Persona = p.ID_Persona", textBox2.Text);
+
+
+            SqlCommand cmd = new SqlCommand(sSel, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            Form f = new Form5(reader, conn);
+            f.Show();
+            
+        }
+    }
+}
---
+++ Entrega 6 OK/DDS-2014-Grupo3/Formularios/Form5.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DDS_2014_Grupo4.Clases.Pantallitas
+{
+    public partial class Form5 : Form
+    {
+        public Form5(SqlDataReader reader, SqlConnection conn)
+        {
+            InitializeComponent();
+            string ID_Jugador = null;
+            while (reader.Read())
+            {
+                textBox1.Text = reader["Nombre"].ToString();
+                textBox2.Text = reader["Apodo"].ToString();
+                textBox3.Text = reader["Handicap"].ToString();
+                textBox6.Text = reader["Fecha_Nacimiento"].ToString();
+                ID_Jugador = reader["ID_Jugador"].ToString();
+            }
+            
+            
+            reader.Close();
+            conn.Close();
+            
+            
+            string sSel = string.Format("select p.Nombre, p.Apellido from [GD2C2014].[dbo].[Persona] p, [GD2C2014].[dbo].[Jugador] j1, [GD2C2014].[dbo].[Amigo] a where "+
+                "j1.ID_Jugador = {0} and j1.ID_Jugador = a.ID_jugador and a.ID_Persona = p.ID_Persona",ID_Jugador);
+            string ConnStr = @"Data Source=localhost\SQLSERVER2008;Initial Catalog=GD2C2014;User ID=gd;Password=gd2014;Trusted_Connection=False;";
+
+            conn = new SqlConnection(ConnStr);
+           
+            SqlCommand cmd = new SqlCommand(sSel, conn);
+            conn.Open();
+            reader = cmd.ExecuteReader();
+            string nombreApellido;
+            while (reader.Read())
+            {
+                nombreApellido = string.Format("{0}, {1}", reader["Apellido"].ToString(), reader["Nombre"].ToString());
+                listBox1.Items.Add(nombreApellido);
+            }
+            reader.Close();
+            conn.Close(); 
+        }
+        public Form5()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
